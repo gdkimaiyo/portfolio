0225f69fcfbaf44722847dc4fabc068631848b0d
GD Kimaiyo <gdkimaiyo@gmail.com>
2021-03-16 20:29:08 +0300

    added NavBar

---
+++ src/components/Base/BaseImage.vue
@@ -0,0 +1,31 @@
+<template>
+  <img
+    :class="className"
+    :alt="img_alt"
+    :width="img_width"
+    :height="img_height"
+    :src="require('../../assets/' + img)"
+  />
+</template>
+
+<script>
+export default {
+  name: "BaseImage",
+
+  components: {},
+
+  props: {
+    className: String,
+    img_alt: String,
+    img: String,
+    img_width: String,
+    img_height: String
+  }
+};
+</script>
+
+<style>
+  .avatar {
+    vertical-align: middle;
+  }
+</style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" class="">
+    <NavBar></NavBar>
+    <div class="container row pt-2 m-auto justify-content-center">
+      <div class="col-md-3">I am a SidebBar</div>
+      <div class="col-md-9">I am the Main Section</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from "./Body/NavBar.vue";
+
+export default {
+  name: "Layout",
+  
+  components: {
+    NavBar
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
