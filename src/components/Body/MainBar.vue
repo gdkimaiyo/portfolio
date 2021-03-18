fd93f263d59c0301c2b64a7f578804ae413e5e16
GD Kimaiyo <gdkimaiyo@gmail.com>
2021-03-18 20:10:28 +0300

    added Main Section

---
+++ src/components/Body/MainBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <h2>I am the main section</h2>
+  </main>
+</template>
+
+<script>
+// import jsonData from "../../mixins/jsonData.js";
+
+export default {
+  name: "MainBar",
+  // mixins: [jsonData],
+
+  components: {}
+};
+</script>
+
+<style>
+.card,
+.card-header {
+  border: none;
+}
+
+.badge {
+  font-size: 14px;
+}
+</style>
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -3,7 +3,7 @@
     <NavBar />
     <div class="container row pt-2 m-auto justify-content-center">
       <div class="col-md-3"><SideBar /></div>
-      <div class="col-md-9">I am the Main Section</div>
+      <div class="col-md-9"><MainBar /></div>
     </div>
   </div>
 </template>
@@ -11,13 +11,15 @@
 <script>
 import NavBar from "./Body/NavBar.vue";
 import SideBar from "./Body/SideBar.vue";
+import MainBar from "./Body/MainBar.vue";
 
 export default {
   name: "Layout",
 
   components: {
     NavBar,
-    SideBar
+    SideBar,
+    MainBar
   }
 };
 </script>
