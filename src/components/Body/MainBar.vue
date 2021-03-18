ca0565d44dd3750617406f37164892172fcf3d6a
GD Kimaiyo <gdkimaiyo@gmail.com>
2021-03-18 20:23:14 +0300

    added Services Section

--- src/components/Body/MainBar.vue
+++ src/components/Body/MainBar.vue
@@ -20,11 +20,24 @@
         </div>
       </div>
     </section>
+
+    <!-- My Services Section -->
+    <section class="shadow mb-5">
+      <h4 class="card-header font-weight-bold">What I do</h4>
+      <div class="card" v-for="(service, index) in services_data" :key="index">
+        <BaseCardBody
+          :card_title="service.service_title"
+          :card_desc="service.service_desc"
+          :show_card_footer="true"
+        />
+      </div>
+    </section>
   </main>
 </template>
 
 <script>
 import BaseAboutMe from "../Base/BaseAboutMe.vue";
+import BaseCardBody from "../Base/BaseCardBody.vue";
 import BaseBadge from "../Base/BaseBadge.vue";
 
 import jsonData from "../../mixins/jsonData.js";
@@ -35,7 +48,8 @@ export default {
 
   components: {
     BaseAboutMe,
-    BaseBadge
+    BaseBadge,
+    BaseCardBody
   }
 };
 </script>
