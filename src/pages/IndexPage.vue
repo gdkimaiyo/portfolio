63da2e25d27f4be3e2a0ed3f27415319427da157
gdkimaiyo <gdkimaiyo@gmail.com>
2023-03-22 22:43:58 +0300

    add landing page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,27 +1,37 @@
 <template>
-  <div id="index-page" class="q-py-lg q-px-md">
-    <div class="text-h5 text-primary">Landing Page</div>
-    <q-separator spaced />
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-      aspernatur asperiores dolorem eveniet fugit. Ipsum, magnam vitae
-      temporibus maiores unde fugiat explicabo autem. Officiis voluptatibus
-      sapiente totam odio, expedita culpa?
-    </p>
+  <div id="index-page">
+    <LandingPage />
+
+    <div class="after-landing q-py-lg q-px-md">
+      <div class="text-h5 text-weight-bold">About Me</div>
+      <q-separator spaced />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+        aspernatur asperiores dolorem eveniet fugit. Ipsum, magnam vitae
+        temporibus maiores unde fugiat explicabo autem. Officiis voluptatibus
+        sapiente totam odio, expedita culpa?
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import LandingPage from "../components/LandingPage.vue";
 
 export default defineComponent({
   name: "IndexPage",
+
+  components: { LandingPage },
 });
 </script>
 
 <style lang="scss" scoped>
 #index-page {
   font-family: "Avenir";
+}
+
+.after-landing {
   width: 100%;
   max-width: 1024px;
   margin: 0 auto;
