e444f61304265bccee424b4ee795f2c31fafe37f
gdkimaiyo <gdkimaiyo@gmail.com>
2023-03-23 16:38:53 +0300

    add scrollTo sections navigation

---
+++ src/components/ProjectsSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="projects" class="projects-section q-my-lg">
+    <div class="text-h5 text-weight-bold">My Projects</div>
+    <q-separator spaced />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+      aspernatur asperiores dolorem eveniet fugit. Ipsum, magnam vitae
+      temporibus maiores unde fugiat explicabo autem. Officiis voluptatibus
+      sapiente totam odio, expedita culpa?
+    </p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ProjectsSection",
+});
+</script>
+
+<style lang="scss" scoped>
+#projects-me {
+  font-family: "Avenir";
+}
+
+@media only screen and (max-width: 575px) {
+  //
+}
+</style>
---
+++ src/components/ServicesSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="services" class="services-section q-my-lg">
+    <div class="text-h5 text-weight-bold">Services</div>
+    <q-separator spaced />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+      aspernatur asperiores dolorem eveniet fugit. Ipsum, magnam vitae
+      temporibus maiores unde fugiat explicabo autem. Officiis voluptatibus
+      sapiente totam odio, expedita culpa?
+    </p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ServicesSection",
+});
+</script>
+
+<style lang="scss" scoped>
+#services {
+  font-family: "Avenir";
+}
+
+@media only screen and (max-width: 575px) {
+  //
+}
+</style>
