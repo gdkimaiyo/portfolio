6cd42281e3730edbbdbd47c44db5847552ddb1da
gdkimaiyo <gdkimaiyo@gmail.com>
2023-03-23 16:38:53 +0300

    add scrollTo sections navigation

---
+++ src/components/TestimonialsSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="testimonials" class="testimonials-section q-my-lg">
+    <div class="text-h5 text-weight-bold">Testimonials</div>
+    <q-separator spaced />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+      aspernatur asperiores dolorem eveniet fugit. Ipsum, magnam vitae
+      temporibus maiores unde fugiat explicabo autem. Officiis voluptatibus
+      sapiente totam odio, expedita culpa?
+    </p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TestimonialsSection",
+});
+</script>
+
+<style lang="scss" scoped>
+#testimonials {
+  font-family: "Avenir";
+}
+
+@media only screen and (max-width: 575px) {
+  //
+}
+</style>
