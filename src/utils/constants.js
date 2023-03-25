923ca3174654f14ff4d7f44556131666cf5d6dff
gdkimaiyo <gdkimaiyo@gmail.com>
2023-03-26 00:11:56 +0300

    add projects section

---
+++ src/utils/constants.js
@@ -0,0 +1,44 @@
+export const PROJECTS = [
+  {
+    leftSided: true,
+    showImage: true,
+    header: "Mini-Netflix",
+    img: "mini-netflix-preview-image-1.png",
+    projectLink: "https://gdkimaiyo.github.io/netflix",
+    codeLink: "https://github.com/gdkimaiyo/netflix",
+    desc: "Mini-Netflix is a Netflix clone application. With this app, users can view movie details like, the movie image, title, year of production, e.t.c. In addition, users can add movies or TV shows they like to their favourites list.",
+    customStyle: {
+      background: "#f0e9ee",
+      border: "0.5px solid #4b135d",
+      btnColor: "#d12e26",
+    },
+  },
+  {
+    leftSided: false,
+    showImage: false,
+    header: "My Portfolio",
+    projectLink: "https://gideonkimaiyo.netlify.app",
+    codeLink: "https://github.com/gdkimaiyo/portfolio",
+    desc: "This website. A typical portfolio website where one can display information about themselves such as about section, the services they offer, showcase their projects, testimonials and how to get in touch to them.",
+    desc2:
+      "I designed this project using Vue Js. Code reusability (Mixins) and components reuse were the core concepts I learnt from designing this portfolio website.",
+    customStyle: {
+      background: "#e9f0f4",
+      border: "0.5px solid #1d4778",
+      btnColor: "#007bff",
+    },
+  },
+  {
+    leftSided: true,
+    showImage: true,
+    header: "TaskManager",
+    img: "mini-netflix-preview-image-1.png",
+    codeLink: "https://github.com/gdkimaiyo/ci3_app",
+    desc: "Project management app where users can create and manage a project and its related tasks. This project was designed with PHP's CodeIgniter3 Framework. Main Concepts I learnt are CRUD and MySQL.",
+    customStyle: {
+      background: "#edf4e3",
+      border: "0.5px solid #2e693b",
+      btnColor: "#006b35",
+    },
+  },
+];
