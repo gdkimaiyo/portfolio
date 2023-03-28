f20c344cc81fb8a8014b05daa2f72cf1f453a838
gdkimaiyo <gdkimaiyo@gmail.com>
2023-03-28 16:28:16 +0300

    add find me social links

--- src/components/FindMe.vue
+++ src/components/FindMe.vue
@@ -1,27 +1,53 @@
 <template>
   <div id="find-me" class="findme-section q-mb-xl">
     <div class="text-h4 text-weight-bold">Find Me</div>
-    <q-separator spaced />
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-      aspernatur asperiores dolorem eveniet fugit. Ipsum, magnam vitae
-      temporibus maiores unde fugiat explicabo autem. Officiis voluptatibus
-      sapiente totam odio, expedita culpa?
-    </p>
+    <div class="q-mt-lg q-mb-md">
+      Feel free to connect. You can find me on the following platforms.
+    </div>
+    <div class="">
+      <a
+        :href="social.link"
+        target="_blank"
+        rel="noopener noreferrer"
+        v-for="(social, index) in socials"
+        :key="index"
+        class="href-link"
+      >
+        <q-chip
+          square
+          text-color="white"
+          :icon="social.logo"
+          clickable
+          :style="{ backgroundColor: social.color }"
+        >
+          {{ social.name }}
+        </q-chip>
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import { SOCIALS } from "../utils/constants";
 
 export default defineComponent({
   name: "FindMe",
+
+  setup() {
+    return {
+      socials: ref(SOCIALS),
+    };
+  },
 });
 </script>
 
 <style lang="scss" scoped>
 #find-me {
   font-family: "Avenir";
+}
+.href-link {
+  text-decoration: none;
 }
 
 @media only screen and (max-width: 575px) {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -132,3 +132,30 @@ export const TESTIMONIALS = [
     initials: "RN",
   },
 ];
+
+export const SOCIALS = [
+  {
+    name: "LinkedIn",
+    logo: "fab fa-linkedin",
+    color: "rgba(46, 100, 188)",
+    link: "https://www.linkedin.com/in/gideon-kimaiyo",
+  },
+  {
+    name: "GitHub",
+    logo: "fab fa-square-github",
+    color: "rgba(38, 41, 46)",
+    link: "https://github.com/gdkimaiyo",
+  },
+  {
+    name: "Medium",
+    logo: "fab fa-medium",
+    color: "rgba(0, 0, 0)",
+    link: "https://medium.com",
+  },
+  {
+    name: "Twitter",
+    logo: "fab fa-square-twitter",
+    color: "rgba(74, 153, 233)",
+    link: "https://twitter.com/gideonGDK",
+  },
+];
