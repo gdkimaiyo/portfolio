36077d5921e54ef276f5dbae2c4951b5b97c591d
gdkimaiyo <gdkimaiyo@gmail.com>
2023-03-28 22:45:45 +0300

    update social links

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -135,22 +135,22 @@ export const TESTIMONIALS = [
 
 export const SOCIALS = [
   {
-    name: "LinkedIn",
-    logo: "fab fa-linkedin",
-    color: "rgba(46, 100, 188)",
-    link: "https://www.linkedin.com/in/gideon-kimaiyo",
-  },
-  {
     name: "GitHub",
     logo: "fab fa-square-github",
     color: "rgba(38, 41, 46)",
     link: "https://github.com/gdkimaiyo",
   },
+  // {
+  //   name: "Medium",
+  //   logo: "fab fa-medium",
+  //   color: "rgba(0, 0, 0)",
+  //   link: "https://medium.com",
+  // },
   {
-    name: "Medium",
-    logo: "fab fa-medium",
-    color: "rgba(0, 0, 0)",
-    link: "https://medium.com",
+    name: "LinkedIn",
+    logo: "fab fa-linkedin",
+    color: "rgba(46, 100, 188)",
+    link: "https://www.linkedin.com/in/gideon-kimaiyo",
   },
   {
     name: "Twitter",
