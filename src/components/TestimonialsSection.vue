a57559a23e9f7e8dbe66bb1931fb319566579573
gdkimaiyo <gdkimaiyo@gmail.com>
2023-03-29 14:38:50 +0300

    update footer content

--- src/components/TestimonialsSection.vue
+++ src/components/TestimonialsSection.vue
@@ -1,39 +1,41 @@
 <template>
-  <div id="testimonials" class="testimonials-section q-mt-lg q-mb-xl">
-    <div class="text-h4 text-weight-bold">Testimonials</div>
-    <div
-      class="q-my-lg"
-      v-for="(testimony, index) in testimonials"
-      :class="{ 'q-my-xl': index > 0 }"
-      :key="index"
-    >
-      <div class="testimony">
-        <div class="">
-          <q-icon name="fas fa-quote-left" color="primary" size="24px" />
-          <span style="padding-left: 6px">{{ testimony.testimony }}</span>
+  <div id="testimonials" class="q-py-lg q-px-md">
+    <div class="testimonials-section">
+      <div class="text-h4 text-weight-bold q-pt-lg q-pb-md">Testimonials</div>
+      <div
+        class="q-my-lg"
+        v-for="(testimony, index) in testimonials"
+        :class="{ 'q-my-xl': index > 0 }"
+        :key="index"
+      >
+        <div class="testimony">
+          <div class="">
+            <q-icon name="fas fa-quote-left" color="primary" size="24px" />
+            <span style="padding-left: 6px">{{ testimony.testimony }}</span>
+          </div>
+          <div class="q-mt-md" v-if="testimony?.testimony2">
+            {{ testimony.testimony2 }}
+          </div>
         </div>
-        <div class="q-mt-md" v-if="testimony?.testimony2">
-          {{ testimony.testimony2 }}
+        <div class="witness q-mt-md">
+          <q-list>
+            <q-item class="q-mt-md q-mb-sm q-pl-none">
+              <q-item-section avatar>
+                <q-avatar color="dark" text-color="white" size="42px">
+                  {{ testimony.initials }}
+                </q-avatar>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label class="text-h6 text-weight-bold">
+                  {{ testimony.name }}
+                </q-item-label>
+                <q-item-label caption>
+                  {{ testimony.profession }}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
         </div>
-      </div>
-      <div class="witness q-mt-md">
-        <q-list>
-          <q-item class="q-mt-md q-mb-sm q-pl-none">
-            <q-item-section avatar>
-              <q-avatar color="dark" text-color="white" size="42px">
-                {{ testimony.initials }}
-              </q-avatar>
-            </q-item-section>
-            <q-item-section>
-              <q-item-label class="text-h6 text-weight-bold">
-                {{ testimony.name }}
-              </q-item-label>
-              <q-item-label caption>
-                {{ testimony.profession }}
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-list>
       </div>
     </div>
   </div>
@@ -57,6 +59,12 @@ export default defineComponent({
 <style lang="scss" scoped>
 #testimonials {
   font-family: "Avenir";
+  background-color: rgba(243, 244, 246);
+}
+.testimonials-section {
+  width: 100%;
+  max-width: 1024px;
+  margin: 0 auto;
 }
 .testimony {
   border-left: 5px solid #007bff;
